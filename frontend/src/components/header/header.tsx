35e2c54e25250444c6fa88788a023c99fc083396
patipanbank <patipanbank1234@gmail.com>
2025-01-15 12:37:01 +0700

    Update header.tsx

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -4,13 +4,13 @@ const Header = () => {
   const userData = userDataString ? JSON.parse(userDataString) : null;
 
   return (
-    <header className="bg-white w-full text-right">
-      <nav className="flex items-center justify-between px-6 py-4 w-full text-right">
+    <header className="bg-white border-b border-gray-200 w-full text-right">
+      <nav className="flex items-center justify-between px-6 py-4 w-fulltext-right">
         {/* Right Side: User Name */}
         <div className="ml-auto text-gray-600">
           {userData && (
             <span>
-              {userData.first_name} {userData.last_name}
+              {userData.firstName} {userData.lastName}
             </span>
           )}
         </div>
