0fbd616e89bd0f386445dd6e71ec87ba928cacc7
patipanbank <patipanbank1234@gmail.com>
2025-01-15 09:25:42 +0700

    .

--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -12,8 +12,8 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex-none z-10">
-        <div className="flex items-center bg-white border-b px-4">
+      <div className="fixed top-0 left-0 right-0 bg-white border-b z-40">
+        <div className="flex items-center px-4">
           <button
             className="p-2 lg:hidden"
             onClick={() => setIsSidebarOpen(true)}
@@ -24,11 +24,11 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
         </div>
       </div>
 
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 overflow-hidden pt-14">
         <div 
           className={`
-            fixed inset-y-0 left-0 z-30 w-64 bg-white transform transition-transform duration-300 ease-in-out
-            lg:relative lg:translate-x-0 lg:flex-shrink-0
+            fixed top-0 left-0 z-50 w-64 h-screen bg-white transform transition-transform duration-300 ease-in-out border-r
+            lg:translate-x-0 lg:relative lg:flex-shrink-0
             ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}
           `}
         >
@@ -37,13 +37,15 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
 
         {isSidebarOpen && (
           <div 
-            className="fixed inset-0 bg-black bg-opacity-50 z-20 lg:hidden"
+            className="fixed inset-0 bg-black bg-opacity-50 z-45 lg:hidden"
             onClick={() => setIsSidebarOpen(false)}
           />
         )}
 
-        <main className="flex-1 flex flex-col overflow-hidden">
-          {children}
+        <main className="flex-1 flex flex-col overflow-hidden w-full">
+          <div className="flex-1 overflow-y-auto">
+            {children}
+          </div>
         </main>
       </div>
     </div>
