4fe2a8c2dec0ac8a241be5fbb8b5f3be925419c8
nalikanon <apisityambangyang@gmail.com>
2025-02-01 00:00:38 +0700

    Update sidebar.tsx

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { FaRobot, FaBars, FaCog, FaHistory, FaSignOutAlt } from 'react-icons/fa';
+import { FaComments, FaBars, FaCog, FaHistory, FaSignOutAlt } from 'react-icons/fa';
 import DarkModeToggle from '../darkmode/DarkModeToggle';
 
 interface SidebarProps {
@@ -55,7 +55,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
             className={`flex items-center px-4 py-2 text-gray-700 dark:text-gray-200 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 
               ${location.pathname === '/mfuchatbot' ? 'bg-gray-100 dark:bg-gray-700' : ''}`}
           >
-            <FaRobot className="w-5 h-5 mr-3" />
+            <FaComments className="w-5 h-5 mr-3" />
             <span>Chat AI</span>
           </Link>
 
