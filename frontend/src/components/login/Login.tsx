8a626a77a81296f4eeef0eb32e3b23b2bab68adb
nalikanon <apisityambangyang@gmail.com>
2025-02-04 23:07:34 +0700

    Update Login.tsx

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -34,7 +34,7 @@ const Login: React.FC = () => {
         {/* Add logo as background */}
         <div className="absolute inset-0 flex items-center justify-center opacity-10 pointer-events-none">
           <img
-            src="/mfu_logo_chatbot.PNG"
+            src="frontend/public/mfu_logo_chatbot.PNG"
             alt="MFU Logo Background"
             className="w-64 h-64 object-contain"
           />
