5b4b1af0db5f28bd0d49107e7439af758869a3e5
BranzX3 <bunzzbranzz@gmail.com>
2025-02-16 21:04:17 +0700

    a

--- frontend/src/components/pages/TrainingDashboard.tsx
+++ frontend/src/components/pages/TrainingDashboard.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { config } from '../../config/config';
 import { FaTrash, FaGlobe, FaFile } from 'react-icons/fa';
 import { CollectionPermission } from '../../types/collection';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface CollectionWithMetadata {
   name: string;
@@ -554,6 +556,20 @@ const TrainingDashboard: React.FC = () => {
           </div>
         )}
       </div>
+
+      {/* Toast container for prettier alert notifications */}
+      <ToastContainer
+        aria-label="Notifications Toast Container"
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 };
