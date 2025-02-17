6ba9c04cc830fdc5fcc34b0244bd0a21ee9c9cc6
patipanbank <patipanbank1234@gmail.com>
2025-02-17 09:53:16 +0700

    Update sidebar.tsx

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
-import { FaComments, FaBars, FaCog, FaHistory, FaSignOutAlt } from 'react-icons/fa';
+// import { FaComments, FaBars, FaCog, FaHistory, FaSignOutAlt } from 'react-icons/fa';
+import { FaComments, FaBars, FaCog, FaSignOutAlt } from 'react-icons/fa';
 import DarkModeToggle from '../darkmode/DarkModeToggle';
 
 interface SidebarProps {
@@ -84,7 +85,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
             </Link>
           )}
 
-          {isStaff && (
+          {/* {isStaff && (
             <Link
               to="/training-history"
               className={`flex items-center px-4 py-2 text-gray-700 dark:text-gray-200 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700
@@ -94,7 +95,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
               <FaHistory className="w-5 h-5 mr-3" />
               <span>History Training</span>
             </Link>
-          )}
+          )} */}
         </nav>
       </div>
 
