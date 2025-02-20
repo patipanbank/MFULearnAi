d0ddd05291ba2873eddc328f7c3190a21593530a
BranzX3 <bunzzbranzz@gmail.com>
2025-02-21 05:17:28 +0700

    Update Collection.ts

--- backend/models/Collection.ts
+++ backend/models/Collection.ts
@@ -1,9 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
 export enum CollectionPermission {
-  PUBLIC = 'public',
-  STAFF_ONLY = 'staff_only', 
+  PUBLIC = 'PUBLIC',
+  STAFF_ONLY = 'STAFF_ONLY',
   PRIVATE = 'private'
+}
+
+export interface Collection extends Document {
+  name: string;
+  permission: CollectionPermission;
+  createdBy: string;
+  // Other properties if needed.
 }
 
 const collectionSchema = new mongoose.Schema({
