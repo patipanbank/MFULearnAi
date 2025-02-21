93d065052dda9fe60f8927f587331b50e5cdf94d
patipanbank <patipanbank1234@gmail.com>
2025-02-21 10:47:14 +0700

    Update header.tsx

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -12,7 +12,8 @@ const Header = () => {
         <div className="ml-auto text-gray-600 dark:text-gray-300">
           {userData && (
             <span>
-              {userData.firstName} {userData.lastName}
+              {/* {userData.firstName} {userData.lastName} */}
+              {userData.username}
             </span>
           )}
         </div>
