7fcf37b430ac983913e4588c939e88265e6225e1
BranzX3 <bunzzbranzz@gmail.com>
2025-02-21 18:22:23 +0700

    Update Model.ts

--- backend/models/Model.ts
+++ backend/models/Model.ts
@@ -1,13 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, Document } from 'mongoose';
 
-export interface IModel {
+export interface IModel extends Document {
+  _id: string; // Use string for _id
   name: string;
   collections: string[]; // The collection names used for vector queries.
   createdBy: string;
   created: Date;
 }
 
-const modelSchema = new mongoose.Schema<IModel>({
+const modelSchema = new Schema<IModel>({
+  // Set _id to type String; a default value is generated as a string version of an ObjectId.
+  _id: { type: String, default: () => new mongoose.Types.ObjectId().toString() },
   name: { type: String, required: true },
   collections: { type: [String], default: [] },
   createdBy: { type: String, required: true },
