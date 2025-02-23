cbd5e29d9b53314026b499b9c39e048c7054e7d2
patipanbank <patipanbank1234@gmail.com>
2025-02-23 17:00:39 +0700

    Update sidebar.tsx

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -116,11 +116,14 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
       } else {
         setChatHistories([]);
       }
+      fetchChatHistories();
     } catch (error) {
       console.error('Error fetching chat histories:', error);
       setChatHistories([]);
     }
+    fetchChatHistories();
   };
+  fetchChatHistories();
 
   useEffect(() => {
     const token = localStorage.getItem('auth_token');
@@ -149,6 +152,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
     return () => {
       window.removeEventListener('chatUpdated', handleChatUpdate);
       window.removeEventListener('chatHistoryUpdated', handleChatHistoryUpdate);
+      fetchChatHistories();
     };
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
