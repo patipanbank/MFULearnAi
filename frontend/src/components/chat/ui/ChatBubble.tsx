2a8f4f5601edc1832ed7afff3f77ac15f46c1535
BranzX3 <bunzzbranzz@gmail.com>
2025-03-21 18:54:32 +0700

    Revert "can"
    This reverts commit f6b5f5d781125cfc801d7c02b2e8d9fae4f90c6b.

--- frontend/src/components/chat/ui/ChatBubble.tsx
+++ frontend/src/components/chat/ui/ChatBubble.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { VscDebugContinue } from "react-icons/vsc";
-import { MdEdit, MdRefresh, MdClose, MdContentCopy } from "react-icons/md";
+import { MdEdit, MdCancel, MdRefresh, MdClose, MdContentCopy } from "react-icons/md";
 import { Message } from '../utils/types';
 import MessageContent from './MessageContent';
 import LoadingDots from './LoadingDots';
@@ -22,6 +22,7 @@ const ChatBubble: React.FC<ChatBubbleProps> = ({
   isLastMessage, 
   isLoading, 
   onContinueClick, 
+  onCancelClick,
   onEditClick,
   onRegenerateClick,
   selectedModel 
