dd98e958422840579d30181487517c3233ec4b57
nalikanon <apisityambangyang@gmail.com>
2025-03-27 18:51:05 +0700

    Update CreateAdmin.tsx

--- frontend/src/components/pages/CreateAdmin.tsx
+++ frontend/src/components/pages/CreateAdmin.tsx
@@ -159,7 +159,7 @@ const CreateAdmin: React.FC = () => {
             placeholder=" Select department"
             isClearable
             className="react-select-container"
-            classNamePrefix="react-select dark:border-gray-600"
+            classNamePrefix="react-select dark:border-gray-600 bg-white dark:bg-gray-700"
             styles={{
               control: (base) => ({
                 ...base,
