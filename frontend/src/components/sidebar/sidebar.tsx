ad09ed604d06ec19b6e1f89bdf294ef3f1b1c456
patipanbank <152005131+patipanbank@users.noreply.github.com>
2025-04-01 14:37:23 +0700

    Update sidebar.tsx

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -194,7 +194,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
       localStorage.clear();
       // for development
       // window.location.href = 'http://localhost:5173/login';
-      window.location.href = 'https://mfulearai.mfu.ac.th';
+      window.location.href = 'https://authsso.mfu.ac.th/adfs/ls/?wa=wsignout1.0';
     } catch (error) {
       console.error('Logout error:', error);
       window.location.href = '/login';
