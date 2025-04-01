b8a15612ab282457eafd1291b3ea747e52e03148
patipanbank <152005131+patipanbank@users.noreply.github.com>
2025-04-01 15:36:38 +0700

    ad

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -192,11 +192,12 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
   const handleLogout = async () => {
     try {
       localStorage.clear();
-      // for development
-      // window.location.href = 'http://localhost:5173/login';
-      window.location.href = 'https://authsso.mfu.ac.th/adfs/ls/?wa=wsignout1.0';
+      
+      // เรียกใช้ API logout ของเราเองแทนการเปลี่ยนเส้นทางโดยตรง
+      window.location.href = `${config.apiUrl}/api/auth/logout/saml`;
     } catch (error) {
       console.error('Logout error:', error);
+      // กรณีเกิด error ให้ไปที่หน้า login
       window.location.href = '/login';
     }
   };
