ed966a7c47a9c14dea1fc4f46590c4642cdde96a
patipanbank <152005131+patipanbank@users.noreply.github.com>
2025-04-01 15:49:16 +0700

    ad

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -189,27 +189,14 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // const handleLogout = async () => {
-  //   try {
-  //     localStorage.clear();
-  //     // for development
-  //     // window.location.href = 'http://localhost:5173/login';
-  //     window.location.href = 'https://authsso.mfu.ac.th/adfs/ls/?wa=wsignout1.0';
-  //   } catch (error) {
-  //     console.error('Logout error:', error);
-  //     window.location.href = '/login';
-  //   }
-  // };
-
   const handleLogout = async () => {
     try {
       localStorage.clear();
-      
-      // เรียกใช้ API logout ของเราเองแทนการเปลี่ยนเส้นทางโดยตรง
-      window.location.href = `${config.apiUrl}/api/auth/logout/saml`;
+      // for development
+      // window.location.href = 'http://localhost:5173/login';
+      window.location.href = 'https://authsso.mfu.ac.th/adfs/ls/?wa=wsignout1.0';
     } catch (error) {
       console.error('Logout error:', error);
-      // กรณีเกิด error ให้ไปที่หน้า login
       window.location.href = '/login';
     }
   };
