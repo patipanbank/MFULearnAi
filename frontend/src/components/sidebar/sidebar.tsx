5b96e17ebb0f8a800f247174f13f5dd14d15b0b9
Nonn <apisityambangyang@gmail.com>
2025-05-20 13:45:09 +0700

    Update sidebar.tsx

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -207,8 +207,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onClose }) => {
       localStorage.clear();
       document.cookie = "MSISAuth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
       
-      // Open login page in new tab
-      window.open('https://mfulearnai.mfu.ac.th/login');
+      // window.open('https://mfulearnai.mfu.ac.th/login');
       
       // Redirect current tab to ADFS logout
       window.location.href = `${config.apiUrl}/api/auth/logout/saml`;
