3fcf8b6c923278c0239de53324848097d3d289d3
Nonn <apisityambangyang@gmail.com>
2025-06-24 18:43:41 +0700

    Update index.tsx

--- frontend/src/pages/ChatPage/index.tsx
+++ frontend/src/pages/ChatPage/index.tsx
@@ -635,7 +635,7 @@ const ChatPage: React.FC = () => {
           {currentSession?.messages.map((msg) => (
             <div
               key={msg.id}
-              className={`flex ${msg.role === 'user' ? 'justify-end' : 'justify-start'}`}
+              className={`flex ${msg.role === 'user' ? 'justify-end' : 'justify-start'} px-4 md:px-16 lg:px-32 xl:px-48`}
             >
               {msg.role !== 'user' && (
                 <div className="flex-shrink-0 mr-2">
@@ -656,8 +656,8 @@ const ChatPage: React.FC = () => {
                 <div
                   className={`max-w-sm sm:max-w-md md:max-w-md lg:max-w-sm xl:max-w-xs w-fit px-3 py-2 rounded-lg ${
                     msg.role === 'user'
-                      ? 'bg-blue-600 text-white ml-2 sm:ml-3 md:ml-4 lg:ml-3 xl:ml-2'
-                      : 'card text-primary mr-2 sm:mr-3 md:mr-4 lg:mr-3 xl:mr-2 ml-8 sm:ml-24 md:ml-48 lg:ml-64 xl:ml-64'
+                      ? 'bg-blue-600 text-white'
+                      : 'card text-primary'
                   }`}
                 >
                   {/* Images */}
