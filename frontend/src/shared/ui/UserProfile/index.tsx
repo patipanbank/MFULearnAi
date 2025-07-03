0d24f9a3944b1ebc7e4e59c50fa8861c2cc1d0e7
BranzX3 <bunzzbranzz@gmail.com>
2025-07-04 04:36:00 +0700

    Revert "Update index.tsx"
    This reverts commit 51f949611ce76a16c57ce866f9c11e7ae0415613.

--- frontend/src/shared/ui/UserProfile/index.tsx
+++ frontend/src/shared/ui/UserProfile/index.tsx
@@ -1,9 +1,9 @@
-import React, { useRef, useEffect, useCallback } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { FiLogOut } from 'react-icons/fi';
 import useAuthStore from '../../../entities/user/store';
 import useUIStore from '../../stores/uiStore';
 
-const UserProfile: React.FC = React.memo(() => {
+const UserProfile: React.FC = () => {
   const dropdownRef = useRef<HTMLDivElement>(null);
   const { user, logoutSAML } = useAuthStore();
   const { openDropdowns, toggleDropdown, closeDropdown } = useUIStore();
@@ -24,12 +24,12 @@ const UserProfile: React.FC = React.memo(() => {
 
     document.addEventListener('mousedown', handleClickOutside);
     return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, [dropdownId]); // Remove closeDropdown from dependencies
+  }, [closeDropdown, dropdownId]);
 
-  const handleCompleteLogout = useCallback(() => {
+  const handleCompleteLogout = () => {
     logoutSAML();
     closeDropdown(dropdownId);
-  }, [logoutSAML, closeDropdown, dropdownId]);
+  };
 
   if (!user) {
     return null;
@@ -147,6 +147,6 @@ const UserProfile: React.FC = React.memo(() => {
       )}
     </div>
   );
-});
+};
 
 export default UserProfile; 
