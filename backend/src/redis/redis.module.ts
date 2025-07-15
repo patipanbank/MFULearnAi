422177c38b546c28c7d7772e72cfc123f6874a99
BranzX3 <bunzzbranzz@gmail.com>
2025-07-15 10:00:10 +0700

    Update redis.module.ts

--- backend/src/redis/redis.module.ts
+++ backend/src/redis/redis.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { RedisService } from './redis.service';
 import { RedisPubSubService } from './redis-pubsub.service';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
-  providers: [RedisPubSubService],
-  exports: [RedisPubSubService],
+  imports: [ConfigModule],
+  providers: [RedisService, RedisPubSubService],
+  exports: [RedisService, RedisPubSubService],
 })
 export class RedisModule {} 
