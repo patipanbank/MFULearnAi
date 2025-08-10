6566a36572e0b013d546ce617744299d0c19ab3d
BranzX3 <bunzzbranzz@gmail.com>
2025-08-11 06:09:37 +0700

    Revert "ฟ"
    This reverts commit 21cc9face65ba3e7faa240646fbec6378e6f0868.

--- frontend/src/shared/ui/EnhancedModal/index.tsx
+++ frontend/src/shared/ui/EnhancedModal/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import type { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { FiX } from 'react-icons/fi';
 import useModalAnimation from '../../hooks/useModalAnimation';
 
