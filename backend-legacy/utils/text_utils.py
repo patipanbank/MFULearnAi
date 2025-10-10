4efa5ef922d463c098813252e88237ccc28bec56
BranzX3 <bunzzbranzz@gmail.com>
2025-10-10 11:15:33 +0700

    a

--- backend-legacy/utils/text_utils.py
+++
@@ -1,7 +0,0 @@
-import tiktoken
-
-def get_token_count(text: str, model: str = "cl100k_base") -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.get_encoding(model)
-    num_tokens = len(encoding.encode(text))
-    return num_tokens 
