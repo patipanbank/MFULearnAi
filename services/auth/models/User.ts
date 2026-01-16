76a3bc3dbe9b370a516874b0ceefbb4ebd7971cb
BranzX3 <bunzzbranzz@gmail.com>
2026-01-16 23:21:09 +0700

    a

--- services/auth/models/User.ts
+++ services/auth/models/User.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import { User as IUser } from '../../shared/types';
+import { User as IUser } from '../../../shared/types';
 
 export interface UserDocument extends Document, Omit<IUser, 'userId'> {
     createdAt: Date;
