c8ade7d3260bd46497a42096a3fd9630ad3cf257
BranzX3 <bunzzbranzz@gmail.com>
2026-01-18 02:17:26 +0700

    Update KnowledgeSelector.vue

--- frontend/src/components/chat/KnowledgeSelector.vue
+++ frontend/src/components/chat/KnowledgeSelector.vue
@@ -120,15 +120,14 @@ onUnmounted(() => {
                 <span class="label-main">{{ currentCollectionName }}</span>
             </div>
 
+            <svg 
+                class="chevron" 
+                :class="{ 'rotate-180': isOpen }"
+                width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            >
+                <polyline points="6 9 12 15 18 9"></polyline>
             </svg>
         </button>
-
-        <!-- Dropdown Menu (Teleported to Body) -->
-        <Teleport to="body">
-            <transition name="dropdown">
-                <div 
-                    v-if="isOpen" 
-                    class="dropdown-menu"
                     :style="{
                         top: `${dropdownPosition.top}px`,
                         left: `${dropdownPosition.left}px`
