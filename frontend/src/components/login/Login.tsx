967c1b292574f596c56466f1302c634d58584ef9
TunDOTio <tuntun.pam3164@gmail.com>
2026-01-29 11:10:16 +0700

    Revert "test"
    This reverts commit 97deb183b349744c089573db208f8d23a138541e.

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -1,8 +1,8 @@
-// import React,{useState} from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { config } from '../../config/config';
 // import axios from 'axios';
 const Login: React.FC = () => {
+  const navigate = useNavigate();
   // const [showLogin, setShowLogin] = useState(false);
   // const [name, setName] = useState('');
   const handleMFUSSOLogin = () => {
@@ -10,7 +10,7 @@ const Login: React.FC = () => {
   };
 
   const handleAdminLogin = () => {
-    window.location.href = import.meta.env.VITE_ADMIN_LOGIN_URL;
+    navigate('/admin/login');
   };
 
   // const navigator = useNavigate();
