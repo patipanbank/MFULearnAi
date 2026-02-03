20e1b14fed0a61688ee64447039679b3d8d4738d
BranzX3 <bunzzbranzz@gmail.com>
2026-02-03 12:31:57 +0700

    a

--- services/knowledge/server.ts
+++ services/knowledge/server.ts
@@ -20,7 +20,8 @@ const ENV_TYPE = process.env.ENV_TYPE || 'TEST';
 
 // Setup
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Database
 mongoose.connect(MONGO_URI)
