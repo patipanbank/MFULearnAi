39c498e02c855839d4334ed49094a7fc6ff2d832
Nonn <apisityambangyang@gmail.com>
2026-02-16 18:19:54 +0700

    แก้ๆขปุ่ม +

--- frontend/src/views/admin/AdminUsers.vue
+++ frontend/src/views/admin/AdminUsers.vue
@@ -6,7 +6,11 @@
         <p class="subtitle">Manage all user accounts and roles.</p>
       </div>
       <button v-if="isSuperAdmin" class="btn-primary" @click="openCreateModal">
-        <span class="icon">+</span> Create User
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <line x1="12" y1="5" x2="12" y2="19"></line>
+          <line x1="5" y1="12" x2="19" y2="12"></line>
+        </svg>
+        Create User
       </button>
     </div>
 
